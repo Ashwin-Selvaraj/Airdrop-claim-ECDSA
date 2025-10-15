7bab34ae60f02bd05b56a17744a0962295b82551
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-10-15 12:43:21 +0530

    Initialized the Repo for ECDSA

---
+++ contracts/Ash_Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract QUEGAToken is ERC20, Ownable {
+    constructor() ERC20("QuestsAndGames Token", "QUEGA") {
+        uint256 initialSupply = 100_000_000_000; // 100 billion
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    // keep mint function commented unless you want minting
+    // function mint(address to, uint256 amount) external onlyOwner {
+    //     _mint(to, amount);
+    // }
+}

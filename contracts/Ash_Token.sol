34870bc5077e40e4bead8162cdc1f39de66cb320
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-10-15 12:45:56 +0530

    Token Contract - ASHTOKEN

--- contracts/Ash_Token.sol
+++ contracts/Ash_Token.sol
@@ -5,14 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract QUEGAToken is ERC20, Ownable {
-    constructor() ERC20("QuestsAndGames Token", "QUEGA") {
+contract AshToken is ERC20, Ownable {
+    constructor() ERC20("Ash Token", "ASH") Ownable(msg.sender) {
         uint256 initialSupply = 100_000_000_000; // 100 billion
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
     // keep mint function commented unless you want minting
-    // function mint(address to, uint256 amount) external onlyOwner {
-    //     _mint(to, amount);
-    // }
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 }
